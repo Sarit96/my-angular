9fa13b95b9b15f33070c4310113d4ab61d0aeddd
Sarit96 <swapna.m@webkorps.com>
2025-05-27 19:26:23 +0530

    added Colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
+import { NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [NgSwitch,NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  task = '';
-  taskList: { id: number; task: string }[] = [];
-
-  addTask() {
-    this.taskList.push({ id: this.taskList.length + 1, task: this.task });
-    this.task = '';
-  }
-
-  onDelete(id: number) {
-    this.taskList = this.taskList.filter((task) => {
-      return task.id !== id;
-    });
-  }
+color = 'red';
 }

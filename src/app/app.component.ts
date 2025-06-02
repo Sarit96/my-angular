11b2470df7e97fa545c56afdf2a80b1ca60c290d
Sarit96 <swapna.m@webkorps.com>
2025-06-02 19:13:28 +0530

    replace login form with user selection and display component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,24 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule,
+} from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    FormsModule
-  ],
+  imports: [UserComponent],
 })
 export class AppComponent {
-  loginForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6)
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ])
-  });
+  userName = 'ironMan';
 
-  onSubmit() {
-    if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-    }
+  onUserChange(user: string) {
+    this.userName = user;
   }
 }

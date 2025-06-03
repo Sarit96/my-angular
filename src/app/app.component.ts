30c2f37cd070cd0ba3f5944095c6c2893c279ea5
Sarit96 <swapna.m@webkorps.com>
2025-06-03 19:14:23 +0530

    Learning Pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import {
 } from '@angular/forms';
 import { UserComponent } from './user/user.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [UserComponent],
+  imports: [CommonModule],
 })
 export class AppComponent {
-  users: string[] = ['Bubun', 'Swapna', 'Sarit', 'Mallick', 'Riyan']
-}
+  title:string = "code Step by Step"
+  date = new Date
+  amount:number=10
+inr: string|undefined;
+ }
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +8,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() user!: string[];
+  @Output() getUsers = new EventEmitter<string[]>();
+  users: string[] = ['Bubun', 'Swapna', 'Sarit', 'Mallick', 'Riyan']
+
+  ngOnInit() {
+    this.getUsers.emit(this.users);
+  }
 }
